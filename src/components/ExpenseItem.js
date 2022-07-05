ac9812d6c542e9878ddbff4cefe552d2c5b2b8ba
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-07-04 21:42:51 -0400

    added expense item

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import './ExpenseItem.css'
 
 const ExpenseItem = () => {
     return (
-    <div>ExpenseItem</div>
+    <div className='expense-item'>
+        <div>Date</div>
+        <h2>item</h2>
+        <div className='expense-itme__description'>
+            <div className='expense-item__price'>$100</div>
+        </div>
+    </div>
     )
 }
-export default ExpenseItem
+export default ExpenseItem;
